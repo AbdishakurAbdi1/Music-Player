bb19b935c717b01d6a1c6839fd973c4cad517b23
Abdishakur Abdi <abdishakurabdi12@hotmail.com>
2026-07-07 19:32:00 +0200

    La til tauri-plugin-store som dependency og registrer den

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_oauth::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
